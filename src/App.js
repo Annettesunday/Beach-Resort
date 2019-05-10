6092868006fc88f326f25fa73c581dd1c09de6bf
annettesunday <annette.odhiambo@andela.com>
2019-05-11 00:09:19 +0300

    ch(Navbar): design Navbar component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Home from "./pages/Home";
 import SingleRoom from "./pages/SingleRoom";
 import Rooms from "./pages/Rooms";
 import Error from "./pages/Error";
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
+    <>
+    <Navbar />
+
     <Switch>
-    <Route exact path="/" component={Home}></Route>
-    <Route exact path="/rooms" component={Rooms}></Route>
-    <Route exact path="/rooms/:slug" component={SingleRoom}></Route>
-    <Route component={Error}></Route>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/rooms" component={Rooms}></Route>
+      <Route path="/rooms/:slug" component={SingleRoom}></Route>
+      <Route component={Error}></Route>
     </Switch>
+    </>
   );
 }
 
