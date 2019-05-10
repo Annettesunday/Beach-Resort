f2806823588572d8b3350b9e771d3693fd6595a9
annettesunday <annette.odhiambo@andela.com>
2019-05-11 00:42:56 +0300

    ch(Herro): add Hero component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
+import Hero from "../components/Hero";
 
 export default function Rooms() {
-  return (
-    <div>
-      Hello from Rooms
-    </div>
-  )
+  return <Hero hero="roomsHero" />
+
 }
