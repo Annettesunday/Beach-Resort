653325618e598acd3aab760dbcc1561eaebd397b
annettesunday <annette.odhiambo@andela.com>
2019-05-13 18:26:23 +0300

    ch(RoomFilter): complete the RoomFilter component

--- src/context.js
+++ src/context.js
@@ -17,7 +17,7 @@ class RoomProvider extends Component {
     minPrice: 0,
     maxPrice: 0,
     minSize: 0,
-    maxSize: 0,
+    maxSize: 1000,
     breakfast: false,
     pets: false
   };
@@ -59,7 +59,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -79,11 +79,41 @@ class RoomProvider extends Component {
       breakfast,
       pets
     } = this.state;
-
+// all the rooms
     let tempRooms = [...rooms];
+    // transform values
+
+    capacity = parseInt(capacity)
+    price= parseInt(price)
+
+
+
+
+// filter by type
     if (type !== "all") {
       tempRooms = tempRooms.filter(room => room.type === type);
     }
+    // filter by capacity
+    if(capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price)
+
+    // filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+    // filter by breakfast
+    if(breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+
+    // filter by pets
+    if(pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
+    // change state
     this.setState({ sortedRooms: tempRooms });
   };
   render() {
